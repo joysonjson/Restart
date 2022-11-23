c580ebc9362905179a3a7773173798bb005e7cde
joyson.ps <joyson.ps@helpshift.com>
2022-11-23 18:11:01 +0530

    Initial Commit

---
+++ first/firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstApp.swift
+//  first
+//
+//  Created by Joyson P S on 23/11/22.
+//
+
+import SwiftUI
+
+@main
+struct firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
